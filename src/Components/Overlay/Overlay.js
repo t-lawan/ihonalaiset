98138462380f6d917999318023f6457d5a85a648
Thomas <thomas.lawanson@gmail.com>
2022-11-20 15:57:04 +0000

    Using vimeo link

--- src/Components/Overlay/Overlay.js
+++ src/Components/Overlay/Overlay.js
@@ -61,7 +61,7 @@ const Overlay = (props) => {
         props.item.type === "VIDEO" &&
         props.item.video_url ? (
           <VideoIFrame
-            src={props.item.video_url}
+            src={"https://player.vimeo.com/video/751273705"}
             title={props.item.title}
             frameBorder="0"
             allowFullScreen={true}
