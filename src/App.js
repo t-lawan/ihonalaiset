7e59a59a33adf15617cde4081a9e0348f5b2cd8f
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2023-03-25 13:19:25 +0000

    Removed unused

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import Home from './Components/Pages/Home';
-import Portfolio from './Components/Pages/Portfolio';
-import Cube from './Components/Pages/Cube';
 
 function App() {
 	return (
